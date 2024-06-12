06db8cfc74766141d5bc5aca9af429b789bff3ec
mateocaranta <mateocaranta@gmail.com>
2024-06-11 22:11:36 -0300

    New Nav

--- src/components/layoutNav/LayoutNav.jsx
+++ src/components/layoutNav/LayoutNav.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
-import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
 import './LayoutNav.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { useNavigate } from 'react-router-dom';
 
 const LayoutNav = () => {
-    const [lightmode, setLightMode] = useState(true);
+    const [scrollYstate, setScrollYstate] = useState(0);
     const [isLogin, setIsLogin] = useState(false);
 
-    const handleChangeLight = () => {
-        setLightMode(!lightmode);
-    };
+    useEffect(() => {
+        const handleScroll = () => {
+            const navbar = document.querySelector('.navbar');
+            if (window.scrollY > scrollYstate) {
+                navbar.classList.add('hidden');
+            } else if (window.scrollY < scrollYstate) {
+                navbar.classList.remove('hidden');
+            }
+            setScrollYstate(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrollYstate]);
+
 
     const navigate = useNavigate();
 
@@ -47,45 +61,31 @@ const LayoutNav = () => {
         navigate("/");
     }
 
-    const handleLogin = () => {
-        setIsLogin(!isLogin);
-    }
     return (
-        <>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container fluid className='containerNav p-0'>
-                    <Navbar.Brand onClick={handleHome}>
-                        <img src="./src/assets/img/logo2.png" height='40' width='40' className='logo' />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarNav" />
-                    <Navbar.Collapse id="navbarNav" className='navCollapse justify-content-md-center'>
-                        <Nav className="me-auto navCenter">
-                            <Nav.Link onClick={handleProductsPage}>Productos</Nav.Link>
-                            <Nav.Link onClick={handleAboutUsPage}>Sobre Nosotros</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link onClick={handleWishListPage}>Lista de Deseados</Nav.Link>
-                            <Nav.Link onClick={handleChangeLight}>
-                                {lightmode ? (<i class="bi bi-moon iconSize"></i>) : (<i class="bi bi-moon-fill iconSize"></i>)}
-                            </Nav.Link>
-                            <Nav.Link onClick={handleBuysPage}>
-                                <i class="bi bi-cart iconSize"></i>
-                            </Nav.Link>
-                            {isLogin ? <NavDropdown title={<i class="bi bi-person-circle iconSize"></i>} id="dropdown-button-dark-example1" align="end">
-                                <NavDropdown.Item onClick={handleBuysRecordsPage}>Compras</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item onClick={handleLogOut}>
-                                    Cerrar Sesion
-                                </NavDropdown.Item>
-                            </NavDropdown> : <Nav.Link onClick={handleLoginPage} className='d-flex'>
-                                <i class="bi bi-person-circle iconSize" onClick={handleLogin}> Login</i>
-                            </Nav.Link>}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-
-        </>
+        <nav className="navbar">
+            <ul>
+                <a className="navbar-item" onClick={handleHome}>
+                    <img className='logo' src="./src/assets/img/logo2.png" alt="" />
+                </a>
+                <li className="navbar-item navli1" onClick={handleProductsPage}>
+                    <p>Productos</p>
+                </li>
+                <li className="navbar-item navli1" onClick={handleAboutUsPage}>
+                    <p>Sobre Nosotros</p>
+                </li>
+                <li className="navbar-item navli1" onClick={handleWishListPage}>
+                    <p>Lista de Deseados</p>
+                </li>
+            </ul>
+            <ul>
+                <li className="navbar-item" onClick={handleBuysPage}>
+                    <p><i className="bi bi-cart iconSize" /></p>
+                </li>
+                <li className="navbar-item" onClick={handleLoginPage}>
+                    <p><i className="bi bi-person-circle iconSize" />Login</p>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
