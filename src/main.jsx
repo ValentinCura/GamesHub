7c373e12cc8dedce834f32a9deac76f93e88c293
mateocaranta <mateocaranta@gmail.com>
2024-06-18 17:25:31 -0300

    Context Creation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ContextProvider from './components/context/ContextProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
 )
