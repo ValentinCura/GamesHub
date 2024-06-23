522b2fc7505cf9286af0e8359a876392f1839526
mateocaranta <mateocaranta@gmail.com>
2024-06-23 16:54:39 -0300

    Creation of user logic

--- src/components/registerPage/Register.jsx
+++ src/components/registerPage/Register.jsx
@@ -1,21 +1,90 @@
-import React, { useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 import './Register.css'
+import { ProductContext } from '../context/ContextProvider';
 
 const Register = () => {
+  // const {users, saveUserDataHandler } = useContext(ProductContext);
+  // const [showPass, setShowPass] = useState(false);
+  // const [errors, setErrors] = useState({
+  //   email: false,
+  //   username: false,
+  //   password: false,
+  // });
+  // const [emailUser, setEmailUser] = useState("");
+  // const [usernameUser, setUsernameUser] = useState("");
+  // const [passwordUser, setPasswordUser] = useState("");
+
+  // const navigate = useNavigate()
+
+  // const handleLogin = () => {
+  //   navigate("/login")
+  // }
+
+  // const handleShowPass = () => {
+  //   setShowPass(!showPass);
+  //   passwordRef.current.type = showPass ? 'text' : 'password';
+  // }
+
+  // const handleEmailChange = (e) => {
+  //   setEmailUser(e.target.value);
+  // };
+
+  // const handleUsernameChange = (e) => {
+  //   setUsernameUser(e.target.value);
+  // };
+
+  // const handlePasswordChange = (e) => {
+  //   setPasswordUser(e.target.value);
+  // };
+
+  // const emailRef = useRef(null);
+  // const usernameRef = useRef(null);
+  // const passwordRef = useRef(null);
+
+  // const registerHandler = (e) => {
+  //   e.preventDefault();
+  //   if (!usernameRef.current.value) {
+  //     usernameRef.current.focus();
+  //     setErrors({ ...errors, username: true })
+  //     alert("Completar username!");
+  //     return;
+  //   }
+  //   if (!passwordRef.current.value) {
+  //     passwordRef.current.focus();
+  //     setErrors({ ...errors, password: true })
+  //     alert("Completar contraseña!");
+  //     return;
+  //   }
+  //   if (!emailRef.current.value) {
+  //     emailRef.current.focus();
+  //     setErrors({ ...errors, email: true })
+  //     alert("Completar email!");
+  //     return;
+  //   }
+
+  //   const userDto = {
+  //     email: emailUser,
+  //     username: usernameUser,
+  //     password: passwordUser
+  //   }
+
+  //   saveUserDataHandler(userDto);
+
+  //   console.log(users);
+
+  //   // navigate("/login");
+  // }
+  const { registerUser } = useContext(ProductContext);
   const [showPass, setShowPass] = useState(false);
-  const [errors, setErrors] = useState({
-    username: false,
-    password: false,
-  });
   const navigate = useNavigate()
 
   const handleLogin = () => {
     navigate("/login")
   }
 
-  const handleShowPass = () =>{
+  const handleShowPass = () => {
     setShowPass(!showPass);
     passwordRef.current.type = showPass ? 'password' : 'text';
   }
@@ -24,27 +93,19 @@ const Register = () => {
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const registerHandler = (e) => {
+  const registerHandler = async (e) => {
     e.preventDefault();
-    if (!usernameRef.current.value) {
-      usernameRef.current.focus();
-      setErrors({ ...errors, username: true })
-      alert("Completar username!");
-      return;
+    try {
+      const userData = await registerUser({
+        email: emailRef.current.value,
+        username: usernameRef.current.value,
+        password: passwordRef.current.value,
+        rol: 'client',
+      });
+      navigate("/login") 
+    } catch (error) {
+      console.error('Registration error:', error);
     }
-    if (!passwordRef.current.value) {
-      passwordRef.current.focus();
-      setErrors({ ...errors, password: true })
-      alert("Completar contraseña!");
-      return;
-    }
-    if (!emailRef.current.value) {
-      emailRef.current.focus();
-      setErrors({ ...errors, email: true })
-      alert("Completar email!");
-      return;
-    }
-    navigate("/")
   }
 
   return (
@@ -52,14 +113,14 @@ const Register = () => {
       <Form onSubmit={registerHandler}>
         <h2>Registrarse</h2>
         <div className="mb-3 groupInputReg" controlId="formBasicEmail">
-          <input type="email" ref={emailRef} placeholder="Email" name="" id="" />
+          <input type="email" ref={emailRef} placeholder="Email" name="" id=""  />
         </div>
         <div className="mb-3 groupInputReg" controlId="formBasicUser">
-          <input type="text" ref={usernameRef} placeholder="Usuario" name="" id="" />
+          <input type="text" ref={usernameRef} placeholder="Usuario" name="" id=""  />
         </div>
         <div className="mb-3 groupInputReg inputPass" controlId="formBasicPass">
-          <input type="password" ref={passwordRef} placeholder="Contraseña" name="" id=""/>
-          <i className={`bi ${showPass ? 'bi-eye-slash': 'bi-eye'}`} onClick={handleShowPass}/>
+          <input type="password" ref={passwordRef} placeholder="Contraseña" name="" id=""  />
+          <i className={`bi ${showPass ? 'bi-eye-slash' : 'bi-eye'}`} onClick={handleShowPass} />
         </div>
         <Button variant="dark" type="submit" className='sessionButton' >
           Registrarse
